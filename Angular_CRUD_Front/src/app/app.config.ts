93e9de38b3da3efa7bc4b0106407e2369ec2a898
dimantha2004 <Dimanthatheekshana12345@gmail.com>
2025-03-13 20:51:06 +0530

    final commit

--- Angular_CRUD_Front/src/app/app.config.ts
+++ Angular_CRUD_Front/src/app/app.config.ts
@@ -4,5 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [ provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient()
+  ]
 };
+function provideHttpClient(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
+  throw new Error('Function not implemented.');
+}
+
--- Angular_CRUD_Front/src/app/page/dashboard/addcustomer/addcustomer.component.ts
+++ Angular_CRUD_Front/src/app/page/dashboard/addcustomer/addcustomer.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-addcustomer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './addcustomer.component.html',
-  styleUrl: './addcustomer.component.css'
+  styleUrls: ['./addcustomer.component.css']
 })
 export class AddcustomerComponent {
+  public customers = [
+    { name: 'Saman', address: 'Panadura', salary: 666 },
+    { name: 'Sama', address: 'Padura', salary: 1212 },
+  ];
 
+  newCustomer = { name: '', address: '', salary: 0 }; 
+
+  addCustomer() {
+    if (this.newCustomer.name && this.newCustomer.address && this.newCustomer.salary !== 0) { 
+     
+      this.customers.push({ ...this.newCustomer });
+      this.newCustomer = { name: '', address: '', salary: 0 }; 
+    } else {
+      alert('Please fill in all fields');
+    }
+  }
+
+  deleteCustomer(index: number) {
+    this.customers.splice(index, 1);
+  }
 }
