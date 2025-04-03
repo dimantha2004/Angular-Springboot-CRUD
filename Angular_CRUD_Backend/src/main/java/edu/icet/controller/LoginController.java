2694114abd24e19255c69ed8095b1edd781b089b
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-04-03 15:23:05 +0530

    login & signin

--- Angular_CRUD_Backend/src/main/java/edu/icet/controller/LoginController.java
+++ Angular_CRUD_Backend/src/main/java/edu/icet/controller/LoginController.java
@@ -1,4 +1,24 @@
 package edu.icet.controller;
 
+import edu.icet.dto.Customer;
+import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin
+@RestController
+@RequestMapping("/api/login")
+@RequiredArgsConstructor
+
 public class LoginController {
+
+    final CustomerService customerService;
+
+
+    @PostMapping("/add")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customerService.addCustomer(customer);
+        return customer;
+    }
 }
