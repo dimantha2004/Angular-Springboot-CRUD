e64b4ac2e7437cb539d4ca789087a5c2c2f461a5
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-04-04 11:15:00 +0530

    entity done

--- Angular_CRUD_Backend/src/main/java/edu/icet/entity/Admin.java
+++ Angular_CRUD_Backend/src/main/java/edu/icet/entity/Admin.java
@@ -1,4 +1,23 @@
 package edu.icet.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "admins")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(name = "phone_number")
+    private Integer phoneNumber;
 }
