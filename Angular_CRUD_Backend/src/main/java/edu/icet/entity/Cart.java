01ce243f4ca6a84133793168d78dd99e330107cc
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-04-17 20:38:40 +0530

    Cart entity Done

---
+++ Angular_CRUD_Backend/src/main/java/edu/icet/entity/Cart.java
@@ -0,0 +1,20 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+}
