e77feab029933c39d6fcc0e0933fb7409cd636c5
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-04-17 20:35:45 +0530

    Cart Controller Done

---
+++ Angular_CRUD_Backend/src/main/java/edu/icet/controller/CartController.java
@@ -0,0 +1,47 @@
+package edu.icet.controller;
+
+import edu.icet.dto.Customer;
+import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+
+public class CartController {
+
+    final CustomerService customerService;
+
+
+    @PostMapping("/add")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customerService.addCustomer(customer);
+        return customer;
+    }
+
+    @GetMapping("/getAll")
+    public List<Customer> getAll(){
+
+
+        return customerService.getAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Customer deleteCustomer(@PathVariable("id") Integer id){
+        customerService.deleteCustomer(id);
+
+        return null;
+    }
+    @PutMapping("/update")
+    public Customer updateCustomer(@RequestBody Customer customer){
+        customerService.updateCustomer(customer);
+        return customer;
+    }
+
+
+
+
+}
