a1f6cbba181adc90ee955f30e047453e9d00226b
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-04-17 20:36:53 +0530

    Cart DTO Done

---
+++ Angular_CRUD_Backend/src/main/java/edu/icet/dto/Cart.java
@@ -0,0 +1,14 @@
+package edu.icet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart{
+    private Integer id;
+    private String name;
+
+}
