444fccb0c6e76d5ceb8e1c107dd6d0de6ac82273
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-04-19 20:12:28 +0530

    Student Repository

--- Angular_CRUD_Backend/src/main/java/edu/icet/repository/StudentRepository.java
+++ Angular_CRUD_Backend/src/main/java/edu/icet/repository/StudentRepository.java
@@ -1,4 +1,7 @@
 package edu.icet.repository;
 
-public class StudentRepository {
+import edu.icet.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
